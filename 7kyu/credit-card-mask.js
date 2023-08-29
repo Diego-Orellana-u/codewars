6fcbece297a8c660d3fdc4869f986604298c9503
Diego Orellana <diego.orellana.git@gmail.com>
2023-08-28 21:55:59 -0400

    added new 7kyu challenge

---
+++ 7kyu/credit-card-mask.js
@@ -0,0 +1,12 @@
+// Usually when you buy something, you're asked whether your credit card number, 
+// phone number or answer to your most secret question is still correct. 
+// However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// My solution:
+
+function maskify(cc) {
+    return cc.slice(-4).padStart(cc.length, '#');  //slice method creates a shallow array with the last 4 digits of the cc string in this case
+                                                   //padStart pads previous string with a new string until it reaches the given length
+  }
