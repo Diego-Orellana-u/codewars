71f98c23f63ab2f31064f103b26d390561822a31
diego orellana <diego.orellana.git@gmail.com>
2024-09-23 18:56:32 -0300

    repeated 8kyu challenge

--- 8kyu/will-you-make-it.js
+++ 8kyu/will-you-make-it.js
@@ -1,26 +1,30 @@
 // You were camping with your friends far away from home, but when it's time to go back,
-// you realize that your fuel is running out and the nearest pump is 50 miles away! 
+// you realize that your fuel is running out and the nearest pump is 50 miles away!
 // You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
 
 // Considering these factors, write a function that tells you if it is possible to get to the pump or not.
 
 // Function should return true if it is possible and false if not.
 
-// My solution:
+// Second Try:
+
+function zeroFuel(distanceToPump, mpg, fuelLeft) {
+  return distanceToPump <= fuelLeft * mpg;
+}
+
+// First Try:
 
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  let totalMiles = mpg * fuelLeft
-  if(distanceToPump > totalMiles){
-    return false
-  }else{
-    return true
+  let totalMiles = mpg * fuelLeft;
+  if (distanceToPump > totalMiles) {
+    return false;
+  } else {
+    return true;
   }
 };
-
 
 // Other interesting solutions:
 
 const zeroFuel2 = (distanceToPump, mpg, fuelLeft) => {
-  return distanceToPump/mpg <= fuelLeft
+  return distanceToPump / mpg <= fuelLeft;
 };
-
