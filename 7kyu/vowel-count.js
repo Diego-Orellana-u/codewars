ed4c0f1afc68963a07d36fa78da089293d9051d1
diego orellana <diego.orellana.git@gmail.com>
2024-09-24 11:32:17 -0300

    added new response to 7kyu challenge

--- 7kyu/vowel-count.js
+++ 7kyu/vowel-count.js
@@ -4,20 +4,29 @@
 
 // The input string will only consist of lower case letters and/or spaces.
 
-// My solution:
+// Second try solution:
+function getCount(str) {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < str.length; i++) {
+    vowels.includes(str[i]) && count++;
+  }
+  return count;
+}
+
+// First try solution:
 
 function getCount(str) {
-    let sum = 0
-    let strg = str.split("")
-    strg.forEach((x,i) => {
-      if(x === "a" || x === "e" || x === "i" || x === "o" || x ==="u")
-      sum += 1
-    })
-    return sum;
+  let sum = 0;
+  let strg = str.split("");
+  strg.forEach((x, i) => {
+    if (x === "a" || x === "e" || x === "i" || x === "o" || x === "u") sum += 1;
+  });
+  return sum;
 }
 
 // Other interesting solutions using match method:
 
 function getCount(str) {
-    return (str.match(/[aeiou]/ig)||[]).length;
-  }
+  return (str.match(/[aeiou]/gi) || []).length;
+}
